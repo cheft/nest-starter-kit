8a2479108b47b0dd1375ad5b93d4c41fa61c9608
Cheft <cncheft@dingtalk.com>
2018-09-29 18:59:19 +0800

    增加用户接口

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -8,6 +8,11 @@ import upload from '../util/upload';
 export class UserController {
   constructor(protected readonly service: UserService) {}
 
+  /**
+   * 获取所有用户
+   * @param query 条件
+   * @param req request
+   */
   @Get()
   @UseGuards(AuthGuard('bearer'))
   list(@Query() query, @Req() req) {
@@ -15,20 +20,105 @@ export class UserController {
     return this.service.list(query);
   }
 
-  @Post()
-  register(@Body() data) {
-    return this.service.create(data);
+  /**
+   * 获取用户信息
+   * @param params 
+   */
+  @Get(':id')
+  detail(@Param() params) {
+    return this.service.detail(params.id);
   }
 
-  @Post('login')
-  login(@Body() data) {
-    return this.service.login(data);
+  @Get('batch')
+  simpleInfo(@Param() params) {
+    return this.service.detail(params.id);
   }
 
+   /**
+   * 更新信息
+   */
+  @Put(':id')
+  update(@Param() params, @Body() data) {
+    return this.service.update(params.id, data);
+  }
+
+  /**
+   * 用户头像上传
+   * @param image 
+   */
   @Post('upload')
   @UseGuards(AuthGuard('bearer'))
   @UseInterceptors(FileInterceptor('image'))
   uploadFile(@UploadedFile() image) {
     return upload(image.originalname, image.buffer);
   }
+
+  /**
+   * 切换关注
+   */
+  @Post('follow/:id')
+  toggleFollow() {
+
+  }
+
+  /**
+   * 获取某用户的关注用户
+   */
+  @Get(':id/follow')
+  getMyFollow() {
+
+  }
+
+  /**
+   * 获取某用户的粉丝
+   */
+  @Get('/:uid/fans')
+  getMyFans() {
+
+  }
+
+  /**
+   * 注册
+   * @param data 用户数据
+   */
+  @Post()
+  register(@Body() data) {
+    return this.service.create(data);
+  }
+
+  /**
+   * 登录
+   * @param data 
+   */
+  @Post('login')
+  login(@Body() data) {
+    return this.service.login(data);
+  }
+
+
+  /**
+   * 获取用户的所有 token
+   */
+  getTokens() {
+
+  }
+
+  /**
+   * 验证 token 获取用户信息
+   */
+  verifyToken() {
+
+  }
+
+  /**
+   * 登出并删除 token
+   */
+  logout() {
+
+  }
+
+  /**
+   * 
+   */
+
 }
